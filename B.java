f426708b8f380b34d2a3040be305f162e2de28e4
ashwiniahire888 <89189805+ashwiniahire888@users.noreply.github.com>
2023-03-28 20:10:22 +0530

    Add files via upload

---
+++ B.java
@@ -0,0 +1,12 @@
+class B extends Asuper
+{
+	public static void main(String[] args)
+	{
+	 B b1 = new B();
+	 b1.test();
+	}
+	public void test()
+	 {
+		System.out.println(super.i);
+	 }
+}
---
+++ Bike.java
@@ -0,0 +1,20 @@
+import java.io.*;
+class Bike
+{
+    void run()
+    {
+        System.out.println("running");
+    }
+}
+    class Splender extends Bike{
+        void run()
+        {
+            System.out.println("running safely with 60 km");
+
+        }
+        public static void main(String args[])
+        {
+            Bike b = new Splender();
+            b.run();
+        }
+    }
