9462050b71b67f59cdfa40ca1e12d7f386724702
ashwiniahire888 <89189805+ashwiniahire888@users.noreply.github.com>
2023-04-05 20:22:02 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class Product{
+
+
+public static void main(String args[]) 
+{
+    int a,b,c;
+a=10;
+b=20;
+c=a*b;
+
+System.out.println("product is:" +c);
+}
+}
