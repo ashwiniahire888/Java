af6bc9dc37a37839f5c060a3a03a0cf01bf65919
ashwiniahire888 <89189805+ashwiniahire888@users.noreply.github.com>
2023-09-19 18:56:06 +0530

    Add files via upload

---
+++ HobbyServ.java
@@ -0,0 +1,66 @@
+import javax.servlet.*;
+
+import javax.servlet.http.*;
+
+import java.io.*;
+ 
+
+public class HobbyServ extends HttpServlet
+
+{
+ 
+ public void doPost(HttpServletRequest req,HttpServletResponse resp)
+  throws ServletException,IOException
+  
+ {
+
+        resp.setContentType("text/html");
+   
+     PrintWriter pw = resp.getWriter(); 
+        String value[] = req.getParameterValues("Hobby");
+
+      Cookie C[]= req.getCookies();
+
+	pw.print("Existing cookies");
+
+	if(C!=null)
+ 
+        {
+	
+  for(int i=0;i<C.length;i++)
+  
+         {
+     
+        pw.print(C[i].getName() + C[i].getValue());
+  
+         }
+  
+       }
+
+
+	Cookie A=new cookie("Hobby", hname);
+	
+if(C!=NULL)
+        
+
+	{
+           
+ for(int i=0;i<C.length;i++)
+    
+         {
+    
+             if(!(C[i].getValue().equals(hname))
+  
+                    resp.addcookie(A);
+      
+          else
+
+                {
+
+                 pw.print("Duplicate Hobby");
+ 
+          return;
+
+     }}
+}
+
---
+++ ShapeDemo.java
@@ -0,0 +1,142 @@
+abstract class Shape
+
+{
+
+ double d1,d2;
+
+ Shape(double d1,double d2)
+
+ {
+
+  this.d1=d1;
+ 
+  this.d2=d2;
+
+ }
+
+ abstract double calc_area();
+
+ abstract double calc_volume();
+
+}
+
+class Sphere extends Shape
+
+{
+
+ Sphere(double d1)
+
+ {
+ 
+  super(d1,0);
+
+ }
+
+ double calc_area()
+
+ {
+
+  return(4*3.14*d1*d1);
+
+ }
+
+ double calc_volume()
+
+ {
+
+  return(4.0/3.0*3.14*d1*d1*d1);
+
+ }
+
+}
+
+class Cone extends Shape
+
+{
+
+ Cone(double d1,double d2)
+ 
+{
+
+  super(d1,d2);
+
+ }
+
+ double calc_area()
+
+ {
+
+  return(3.14*d1*(d1+d2));
+
+ }
+
+ double calc_volume()
+
+ {
+
+  return(3.14*d1*d1*d2/3.0);
+
+ }
+
+}
+
+class Cylinder extends Shape
+
+{
+
+ Cylinder(double d1,double d2)
+
+ {
+
+  super(d1,d2);
+
+ }
+
+ double calc_area()
+
+ {
+
+  return(2*3.14*d1*d2);
+
+ }
+
+ double calc_volume()
+
+ {
+
+  return(3.14*d1*d1*d2);
+
+ }
+
+}
+
+class ShapeDemo
+
+{
+
+ public static void main(String args[])
+
+ {
+
+  Shape S=new Sphere(6.0);
+  
+System.out.println("Area is:" +S.calc_aera());
+
+  System.out.println("Volume is:" +S.calc_volume());
+
+  S=new Cone(7.0,9.0);
+
+  System.out.println("Area is:" +S.calc_aera());
+
+  System.out.println("Volume is:" +S.calc_volume());
+
+  S=new Cylinder(7.2,8.2);
+
+  System.out.println("Area is:" +S.calc_aera());
+
+  System.out.println("Volume is:" +S.calc_volume());
+   
+ }
+
+}
+
